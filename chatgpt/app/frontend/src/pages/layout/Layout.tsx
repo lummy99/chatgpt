c958db8d703a046811082efc2131fa81006d572e
Faturoti <pelumifaturoti@gmail.com>
2023-05-29 20:03:00 -0400

    first

---
+++ chatgpt/app/frontend/src/pages/layout/Layout.tsx
@@ -0,0 +1,50 @@
+import { Outlet, NavLink, Link } from "react-router-dom";
+
+import github from "../../assets/github.svg";
+
+import styles from "./Layout.module.css";
+
+const Layout = () => {
+    return (
+        <div className={styles.layout}>
+            <header className={styles.header} role={"banner"}>
+                <div className={styles.headerContainer}>
+                    <Link to="/" className={styles.headerTitleContainer}>
+                        <h3 className={styles.headerTitle}>Brookfield Asset Management</h3>
+                    </Link>
+                    <nav>
+                        <ul className={styles.headerNavList}>
+                            <li>
+                                <NavLink to="/" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Query your company's data
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <NavLink to="/qa" className={({ isActive }) => (isActive ? styles.headerNavPageLinkActive : styles.headerNavPageLink)}>
+                                    Ask a question
+                                </NavLink>
+                            </li>
+                            <li className={styles.headerNavLeftMargin}>
+                                <a href="https://aka.ms/entgptsearch" target={"_blank"} title="Github repository link">
+                                    <img
+                                        src={github}
+                                        alt="Github logo"
+                                        aria-label="Link to github repository"
+                                        width="20px"
+                                        height="20px"
+                                        className={styles.githubLogo}
+                                    />
+                                </a>
+                            </li>
+                        </ul>
+                    </nav>
+                    <h4 className={styles.headerRightText}>Brookfield's Data + Cognitive Search Powered by Infra and Infosec team </h4>
+                </div>
+            </header>
+
+            <Outlet />
+        </div>
+    );
+};
+
+export default Layout;
